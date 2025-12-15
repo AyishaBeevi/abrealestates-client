e207cf6ff41912c2749cef0c7f348553017606c9
AyishaBeevi <ayshhabeevi@gmail.com>
2025-12-15 21:17:29 +0530

    axios update

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -8,7 +8,19 @@ if (!API_URL) {
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true,
-})
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
