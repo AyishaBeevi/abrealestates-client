d85ec784cf11e086361a22768cdca78e8eca608d
AyishaBeevi <ayshhabeevi@gmail.com>
2025-12-15 19:47:54 +0530

    api url fix

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -1,19 +1,14 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-  withCredentials: true,
-});
+const API_URL = import.meta.env.VITE_API_URL;
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+if (!API_URL) {
+  throw new Error("VITE_API_URL is not defined");
+}
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+})
 
 export default api;
